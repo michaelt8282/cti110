411377bb60687ccaccd4e9e5cfc442fe0c5cb454
michaelt8282 <michaelt8282@student.faytechcc.edu>
2024-10-08 18:24:45 +0000

    P1HW2

---
+++ P1HW2.py
@@ -0,0 +1,29 @@
+# CTI 110
+# P1HW2
+# T Michael
+# 10/8/24
+
+# This program does a travel budget
+"""
+3. Ask user to enter their budget
+4. Ask user to enter travel destination
+5. Ask user for amount they will spend on gas
+6. Ask user for amount they will spend on accommodation
+7. Ask user for amount they will spend on food
+8. Add expenses
+9. Subtract expenses from budget
+10. Display results
+"""
+print("💀Trip Calculator💀")
+# Ask user to enter their budget
+budget = float(input("What is your travel budget? $ "))
+# Where are they going
+destination = input("Where are you going?")
+budget = float(input("How much do you think you will spend on gas? $ "))
+# What is your budget for gas
+budget = float(input("Approximately, how much will you need for accomadation/hotel? $ " ))
+# How much will you need for accomadations
+budget = float(input("How much do you need for food? $ "))
+# How much do you need for food
+print("💸Travel Expenses💸")
+print("Location: Missouri")
